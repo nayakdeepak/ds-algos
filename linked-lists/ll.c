de264876c2693ebdd1c90fc58ffeac80a7175298
Deepak Nayak <nayakd@gmail.com>
2022-02-12 15:10:03 +0530

    Added code for linked list insert and delete (before a given value)

--- linked-lists/ll.c
+++ linked-lists/ll.c
@@ -69,6 +69,84 @@ int ll_remove(ll_node_t *head)
 	
 	return a;
 }
+
+// insert before given value
+void ll_insert_before(ll_node_t *head, int x, int before)
+{
+	ll_node_t *tmp, *prev, *new;
+	
+	printf("INSERT: ");
+	
+	tmp = head;
+	
+	while(tmp->next != NULL)
+	{
+		prev = tmp;
+		tmp = tmp->next;
+		if (tmp->a == before)
+			break;
+	}
+	
+	if (tmp->a != before)
+	{
+		printf("%d doesn't exist in list!\n", before);
+		return;
+	}
+	
+	new = (ll_node_t *)malloc(sizeof(ll_node_t));
+	new->a = x;
+	new->next = tmp;
+
+	if (tmp == head)
+		tmp->next = new;
+	else
+		prev->next = new;
+	
+	printf("%d inserted before %d\n", x, before);
+	
+	return;	
+}
+
+// delete item before
+// p1 -- p2 -- p3 (holds before)
+int ll_delete_before(ll_node_t *head, int before)
+{
+	int x;
+	ll_node_t *p1, *p2, *p3;
+	
+	printf("DELETE: ");
+	
+	if ((head->next)->a == before)
+	{
+		printf("Can't delete HEAD!\n");
+		return -1;
+	}
+	
+	p1 = head;
+	while(p1->next->next != NULL)
+	{
+		p2 = p1->next;
+		p3 = p2->next;
+		if (p3->a == before)
+			break;
+		p1 = p1->next;
+	}
+	
+	if (p3->a != before)
+	{
+		printf("%d doesn't exist in list!\n", before);
+		return -1;
+	}
+	
+	x = p2->a;
+	
+	p1->next = p2->next;
+	free(p2);
+	
+	printf("%d deleted, was before %d\n", x, before);
+	
+	return x;	
+}
 	
 int main(int argc, char *argv[])
 {
@@ -84,6 +162,36 @@ int main(int argc, char *argv[])
 	ll_print(&head);
 	ll_add(895, &head);
 	ll_print(&head);
+
+	ll_insert_before(&head, 100, 45);
+	ll_print(&head);
+	
+	ll_insert_before(&head, 1145, 23);
+	ll_print(&head);
+	
+	ll_insert_before(&head, 2000, 895);
+	ll_print(&head);
+	
+	ll_insert_before(&head, 199, 678);
+	ll_print(&head);
+	
+	int a = ll_delete_before(&head, 1145);
+	ll_print(&head);
+	
+	a = ll_delete_before(&head, 23);
+	ll_print(&head);
+	
+	a = ll_delete_before(&head, 100);
+	ll_print(&head);
+	
+	a = ll_delete_before(&head, 1290);
+	ll_print(&head);
+	
+	a = ll_delete_before(&head, 895);
+	ll_print(&head);
+	
+	a = ll_delete_before(&head, 1290);
+	ll_print(&head);
 	
 	printf("%d removed\n", ll_remove(&head));
 	ll_print(&head);
@@ -92,7 +200,7 @@ int main(int argc, char *argv[])
 	
 	printf("%d removed\n", ll_remove(&head));
 	ll_print(&head);
-	
+
 	printf("%d removed\n", ll_remove(&head));
 	ll_print(&head);
 	
@@ -101,6 +209,7 @@ int main(int argc, char *argv[])
 	
 	printf("%d removed\n", ll_remove(&head));
 	ll_print(&head);
+	
 	
 	return 0;
 }
