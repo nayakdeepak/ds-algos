3b5fc5ee26b9d98b82db4c970184c013a7343ca2
Deepak Nayak <nayakd@gmail.com>
2022-02-13 09:20:18 +0530

    Added insert and delete for dll

--- linked-lists/dll.c
+++ linked-lists/dll.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 // the dll is circular - head --- through the list -- back to head
+
 typedef struct dll_node
 {
 	int a;
@@ -15,10 +16,89 @@ enum
 	reverse
 };
 
-// add a node at the end
-void dll_add(int x, dll_node_t *head)
+// insert a node before a value
+void dll_insert_before(int x, int before, dll_node_t *head)
 {
 	dll_node_t *tmp, *new;
+	
+	printf("Insert before: ");
+	
+	tmp = head;
+	while(tmp->next != head)
+	{
+		if (tmp->a == before)
+			break;
+		tmp = tmp->next;
+	}
+	
+	if ((tmp->next == head)&&(tmp->a!=before))
+	{
+		printf("%d does not exist in the list!\n", before);
+		return;
+	}
+	
+	new = (dll_node_t *)malloc(sizeof(dll_node_t));
+	new->a = x;
+	new->next = tmp;
+	new->prev = tmp->prev;
+	
+	tmp->prev->next = new;
+	tmp->prev = new;
+	
+	printf("%d inserted before %d\n", x, before);
+	
+	return;
+}
+
+// delete a node before a value
+int dll_delete_before(int before, dll_node_t *head)
+{
+	dll_node_t *tmp, *pre, *prev;
+	int x;
+	
+	printf("Delete Before: ");
+	
+	if (head->next->a == before)
+	{
+		printf("Can't delete before first node!\n");
+		return -1;
+	}
+	
+	tmp=head;
+	while (tmp->next != head)
+	{
+		if (tmp->a==before)
+			break;
+		tmp = tmp->next;
+	}
+	
+	if ((tmp->next == head)&&(tmp->a!=before))
+	{
+		printf("%d doesn't exist in the list!\n", before);
+		return -1;
+	}
+	
+	prev = tmp->prev;
+	pre = prev->prev;
+	
+	x = prev->a;
+	
+	pre->next = tmp;
+	tmp->prev = pre;
+	
+	free(prev);
+	
+	printf("%d deleted before %d\n", x, before);
+	
+	return -1;
+}
+
+// add a node at the end
+void dll_add_last(int x, dll_node_t *head)
+{
+	dll_node_t *tmp, *new;
+	
+	printf("Add Last: ");
 	
 	tmp = head;
 	while (tmp->next != head)
@@ -37,11 +117,33 @@ void dll_add(int x, dll_node_t *head)
 	return;
 }
 
+// add a node at the first
+void dll_add_first(int x, dll_node_t *head)
+{
+	dll_node_t *tmp, *new;
+	
+	printf("Add First: ");
+	
+	new = (dll_node_t *)malloc(sizeof(dll_node_t));
+	new->a = x;
+	new->prev = head;
+	new->next = head->next;
+	
+	head->next->prev = new;
+	head->next = new;
+	
+	printf("%d added\n", x);
+	
+	return;
+}
+
 // remove the last node
-int dll_remove(dll_node_t *head)
+int dll_remove_last(dll_node_t *head)
 {
 	int a;
 	dll_node_t *tmp, *prev;
+	
+	printf("Remove Last: ");
 	
 	tmp = head;
 	if (tmp->next == head || tmp->prev == head)
@@ -66,6 +168,8 @@ int dll_remove_first(dll_node_t *head)
 {
 	int a;
 	dll_node_t *tmp, *next;
+	
+	printf("Remove First: ");
 	
 	tmp = head->next;
 	
@@ -118,10 +222,39 @@ int main(int argc, char *argv[])
 	
 	dll_print(forward, &head);
 	dll_print(reverse, &head);
-	dll_add(10, &head);
-	dll_add(24, &head);
-	dll_add(1021, &head);
-	dll_add(2345, &head);
+	dll_add_last(10, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	dll_add_last(24, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	dll_add_first(1021, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	dll_add_last(2345, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	
+	dll_insert_before(1500, 2400, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	dll_insert_before(100, 24, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	dll_insert_before(23, 2345, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	
+	dll_delete_before(1021, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	dll_delete_before(678, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	dll_delete_before(100, &head);
+	dll_print(forward, &head);
+	dll_print(reverse, &head);
+	dll_delete_before(2345, &head);
 	dll_print(forward, &head);
 	dll_print(reverse, &head);
 	
@@ -129,27 +262,25 @@ int main(int argc, char *argv[])
 	dll_print(forward, &head);
 	dll_print(reverse, &head);
 	
-	printf("%d removed\n", dll_remove(&head));
+	printf("%d removed\n", dll_remove_last(&head));
 	dll_print(forward, &head);
 	dll_print(reverse, &head);
 		
-	printf("%d removed\n", dll_remove(&head));
+	printf("%d removed\n", dll_remove_last(&head));
 	dll_print(forward, &head);
 	dll_print(reverse, &head);
 		
-	printf("%d removed\n", dll_remove(&head));
+	printf("%d removed\n", dll_remove_first(&head));
 	dll_print(forward, &head);
 	dll_print(reverse, &head);
 		
-	printf("%d removed\n", dll_remove(&head));
+	printf("%d removed\n", dll_remove_last(&head));
 	dll_print(forward, &head);
 	dll_print(reverse, &head);
 	
-	printf("%d removed\n", dll_remove(&head));
+	printf("%d removed\n", dll_remove_last(&head));
 	dll_print(forward, &head);
 	dll_print(reverse, &head);
 	
-	
-		
 	return 0;
 }
